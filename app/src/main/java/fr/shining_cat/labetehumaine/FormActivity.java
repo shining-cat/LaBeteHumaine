3de98908bb2c6fb2f7fd4b020640a68f40c52951
shining-cat <sh1v4@shining-cat.fr>
2017-07-19 15:29:32 +0200

    implemented empty form activity that opens when form button is tapped, with back to main activity button

--- app/src/main/java/fr/shining_cat/labetehumaine/FormActivity.java
+++ app/src/main/java/fr/shining_cat/labetehumaine/FormActivity.java
@@ -2,13 +2,23 @@ package fr.shining_cat.labetehumaine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 
 public class FormActivity extends AppCompatActivity {
+
+    private final String TAG = "LOGGING::" + this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        if(MainActivity.DEBUG) {
+            Log.i(TAG, "onCreate");
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
